c2afc66f8d6210167e7dbc0728acde8fda27db36
Gautham Rajsimha Pulipati <pulipati.g@gmail.com>
2021-08-09 21:16:57 -0400

    sharing done

---
+++ frontend/src/Components/BlogId.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import BlogServices from '../services/blogs.services';
+import { withRouter } from "react-router";
+import { Container } from "react-bootstrap";
+import SingleBlog from "./SingleBlog";
+import Loading from "./Loading";
+
+class BlogId extends Component{
+    constructor(props){
+        super(props)
+        
+        this.state ={
+            blog: undefined,
+            id : this.props.match.params.id,
+            loaded: false
+        }
+    }
+
+    componentDidMount(){
+        console.log('moun')
+        BlogServices.getPostById(this.props.match.params.id).then(
+            (response) => {
+                console.log(response)
+                const blog = response
+                blog.commentVisibility='none'
+                blog.buttonText = 'See Comments'
+                blog.currentComment=''
+                this.setState({blog:blog, loaded:true})
+            }
+        )
+    }
+
+    render(){
+        console.log(this.state.id)
+        return(
+            <Container>
+                {
+                    this.state.loaded === false
+                    ? <Loading />
+                    : <SingleBlog blog = {this.state.blog}/>
+                }
+            </Container>
+        )
+    }
+}
+
+export default BlogId
