42efa44337f7a47d607568d17138fc18098c3f40
sharmaankita-evry <sharma.ankit@northeastern.edu>
2021-08-11 02:07:36 -0400

    add css for cusines

--- frontend/src/Components/Cusines/Cuisine.js
+++ frontend/src/Components/Cusines/Cuisine.js
@@ -1,14 +1,16 @@
-import React, {Component} from "react";
-import { Container, Row, Image, Carousel, Col, Form, Dropdown, Card, Button, CardGroup,Header } from "react-bootstrap";
+import React, { Component } from "react";
+import { Container, Row, Image, Carousel, Col, Form, Dropdown, Card, Button, CardGroup, Header } from "react-bootstrap";
 import "./Cusines.css";
+//import img from './foodblog.jpg';
+
 
 const DOCUMENU_API_KEY = "cff91f44c684a03c1b4d509c5085c946";
 const SPOONACULAR_API_KEY = "adef6fa141fa432796e6d6a8aaa8d63b";
 
-class Cuisine extends Component{
+class Cuisine extends Component {
 
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
         this.state = {
             restaurants: [],
             recipes: []
@@ -16,14 +18,14 @@ class Cuisine extends Component{
     }
 
     getRestaurantData = async (e) => {
-        const api_call = await fetch(`https://api.documenu.com/v2/restaurants/search/geo?lat=${this.props.lat}&lon=${this.props.lon}&distance=10&cuisine=${this.props.cuisine}&key=${DOCUMENU_API_KEY}`);
+        const api_call = await fetch(`https://api.documenu.com/v2/restaurants/search/geo?lat=${this.props.lat}&lon=${this.props.lon}&distance=9&cuisine=${this.props.cuisine}&key=${DOCUMENU_API_KEY}`);
         const result = await api_call.json();
         this.setState({ restaurants: result.data });
         console.log(this.state.restaurants);
     }
 
     getSpoonacularRecipes = async (e) => {
-        const api_call = await fetch(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${this.props.cuisine}&number=5&addRecipeInformation=true&apiKey=${SPOONACULAR_API_KEY}`);
+        const api_call = await fetch(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${this.props.cuisine}&number=7&addRecipeInformation=true&apiKey=${SPOONACULAR_API_KEY}`);
         const result = await api_call.json();
         this.setState({ recipes: result.results });
     }
@@ -35,51 +37,50 @@ class Cuisine extends Component{
         }
     }
 
-    render( props ) {
-        return(
-        <div className = "Cusines">
-        <Row className="rows">
-          <Col className="columns" md={6}>
-            { this.state.restaurants.map((res) => {
-                return <div className = "restaurant">
-                    <Card style={{ width: '100%' }}>
-                        <Card.Body>
-                        <Card.Title>{ res.restaurant_name }</Card.Title>
-                        <Card.Text>
-                            <p>Address: {res.address.formatted}</p>
-                            <p>Contact: {res.restaurant_phone}</p>
-                        </Card.Text>
-                        </Card.Body>
-                        <Card.Footer className="text-muted">
-                            <a class="btn btn-primary" href={ res.restaurant_website } role="button" target="_blank">Visit website</a>
-                        </Card.Footer>
-                    </Card>
-                    <br></br>
-                </div>
-            })}
-            <br></br>
-          </Col>
+    render(props) {
+        return (
+            <div className="Cusines">
+                
+                <Row className="rows">
+                    <Col className="columns" md={6}>
+                        {this.state.restaurants.map((res) => {
+                            return <div className="restaurant">
+                                
+                                <Card style={{ width: '100%' }}>
+                                    <Card.Body>
+                                        <Card.Title>{res.restaurant_name}</Card.Title>
+                                        <Card.Text>
+                                            <p>Address: {res.address.formatted}</p>
+                                            <p>Contact: {res.restaurant_phone}</p>
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer className="text-muted">
+                                        <a class="btn btn-primary" href={res.restaurant_website} role="button" target="_blank">Visit website</a>
+                                    </Card.Footer>
+                                </Card>
+                                <br></br>
+                            </div>
+                        })}
+                        <br></br>
+                    </Col>
 
-          <Col className="columns" lg={6}>
-          { this.state.recipes.map((recipe) => {
-            return <div className = "recipe"><Card style={{ width: '100%' }}>
-                <Card.Img variant="top" src="holder.js/100px180" src={ recipe.image } />
-                <Card.Body>
-                <Card.Title>{ recipe.title }</Card.Title>
-                <Card.Text>
-                    { recipe.summary }
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted">
-                    <a class="btn btn-primary" href={ recipe.sourceUrl } role="button" target="_blank">Visit website</a>
-                </Card.Footer>
-            </Card>
-            <br></br>
+                    <Col className="recipe" lg={6}>
+                        {this.state.recipes.map((recipe) => {
+                            return <div className="recipe"><Card style={{ width: '100%' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" src={recipe.image} />
+                                <Card.Body>
+                                    <Card.Title>{recipe.title}</Card.Title>
+                                    <Card.Text>
+                                        <div dangerouslySetInnerHTML={{ __html: recipe.summary }} />
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                                <br></br>
+                            </div>
+                        })}
+                    </Col>
+                </Row>
             </div>
-            })}
-          </Col>
-        </Row>
-        </div>
         )
     }
 }
--- frontend/src/Components/Food.js
+++ frontend/src/Components/Food.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Container, Row, Image, Carousel, Col, Form, Dropdown, Card, Button, CardGroup } from "react-bootstrap";
 import Cuisine from "./Cusines/Cuisine";
-import  "./Food";
-
 
 const SPOONACULAR_API_KEY = "adef6fa141fa432796e6d6a8aaa8d63b";
 
@@ -25,25 +23,25 @@ class Food extends Component {
   }
 
   success(pos) {
-    this.setState({latitude: pos.coords.latitude});
-    this.setState({longitude: pos.coords.longitude});
+    this.setState({ latitude: pos.coords.latitude });
+    this.setState({ longitude: pos.coords.longitude });
   }
 
   error(err) {
-    this.setState({latitude: "42.360081"});
-    this.setState({longitude: "-71.058884"});
+    this.setState({ latitude: "42.360081" });
+    this.setState({ longitude: "-71.058884" });
   }
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(this.success, this.error);
   }
-   
+
 
   getSpoonacularRecipes = async (e) => {
     const api_call = await fetch(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${this.props.cuisine}&number=10&addRecipeInformation=true&apiKey=${SPOONACULAR_API_KEY}`);
     const result = await api_call.json();
     this.setState({ recipes: result.results });
-}
+  }
 
   render() {
 
@@ -58,41 +56,16 @@ class Food extends Component {
             <Dropdown.Menu >
               <Dropdown.Item eventKey="italian">Italian</Dropdown.Item>
               <Dropdown.Item eventKey="mexican">Mexican</Dropdown.Item>
-              <Dropdown.Item eventKey="vegan">Vegan</Dropdown.Item>
               <Dropdown.Item eventKey="indian">Indian</Dropdown.Item>
               <Dropdown.Item eventKey="korean">Korean</Dropdown.Item>
               <Dropdown.Item eventKey="american">American</Dropdown.Item>
             </Dropdown.Menu>
-          </Dropdown>       
-        <br></br>      
-      </Row>
+          </Dropdown>
+          <br></br>
+        </Row>
 
         <br></br>
-        <Cuisine cuisine={this.state.selectValue} lat={this.state.latitude} lon={this.state.longitude}/>
-       
-        <Row>
-          <div className = "Food_recipe">
-            <Col className="columns">
-              {this.state.recipes.map((recipe) => {
-                return <div><Card className = "Recipe_card" style={{ width: '100%' }} >
-                  <Card.Img variant="top" src="holder.js/100px180" src={recipe.image} />
-                  <Card.Body>
-                    <Card.Title>{recipe.title}</Card.Title>
-                    <Card.Text>
-                      {recipe.summary}
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer className="text-muted">
-                    <a class="btn btn-primary" href={recipe.sourceUrl} role="button" target="_blank">Visit website</a>
-                  </Card.Footer>
-                </Card>
-                  <br></br>
-                </div>
-              })}
-            </Col>
-          </div>
-
-        </Row>
+        <Cuisine cuisine={this.state.selectValue} lat={this.state.latitude} lon={this.state.longitude} />
 
       </Container>
 
