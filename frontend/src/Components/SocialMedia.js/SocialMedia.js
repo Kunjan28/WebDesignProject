c73bf128e51eb77eca7cfc160cbf614eead5050d
Kunjan28 <gala.k@northeastern.edu>
2021-08-10 17:00:25 -0700

    link open to other tab

--- frontend/src/Components/SocialMedia.js/SocialMedia.js
+++ frontend/src/Components/SocialMedia.js/SocialMedia.js
@@ -6,18 +6,18 @@ function SocialMedia() {
         <div className="footer-icon-sidebar">
             <ul className="navbar-nav">
                 <li className="nav-item inline-block">
-                    <a href="https://github.com/Kunjan28/WebDesignProject" aria-label="Github" className="nav-link text-github"><i
+                    <a href="https://github.com/Kunjan28/WebDesignProject" target="_blank" aria-label="Facebook" className="nav-link text-github"><i
                         className="fa fa-github"></i></a></li>
-                <li className="nav-item inline-block"><a href="https://www.facebook.com/kunjan.gala.9" aria-label="Insagram" className="nav-link"><i className="fa fa-facebook"
+                <li className="nav-item inline-block"><a href="https://www.facebook.com/" target="_blank" aria-label="Insagram" className="nav-link"><i className="fa fa-facebook"
                 ></i></a>
                 </li>
                 <li className="nav-item inline-block">
-                    <a href="https://github.com/Kunjan28/WebDesignProject" aria-label="Github" className="nav-link text-github"><i
+                    <a href="https://twitter.com/login" aria-label="Twitter"  target="_blank" className="nav-link text-github"><i
                         className="fa fa-twitter"></i></a></li>
-                <li className="nav-item inline-block"><a href="https://www.instagram.com/kunjan.gala/" aria-label="Insagram" className="nav-link"><i className="fa fa-instagram"
+                <li className="nav-item inline-block"><a href="https://www.instagram.com/" target="_blank" aria-label="Instagram" className="nav-link"><i className="fa fa-instagram"
                 ></i></a>
                 </li>
-                <li className="nav-item inline-block"><a href="https://www.linkedin.com/in/kunjangala28/" aria-label="LinkedIn" className="nav-link"><i className="fa fa-linkedin"
+                <li className="nav-item inline-block"><a href="https://www.linkedin.com/" target="_blank" aria-label="LinkedIn" className="nav-link"><i className="fa fa-linkedin"
                 ></i></a>
                 </li>
             </ul>
