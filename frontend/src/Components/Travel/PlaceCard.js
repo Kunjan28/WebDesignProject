7e64a39ac56980b7e8dda228100544892b5c62fd
JayaKalele <kalele.jaya@gmail.com>
2021-08-10 18:57:50 -0700

    travel adj

--- frontend/src/Components/Travel/PlaceCard.js
+++ frontend/src/Components/Travel/PlaceCard.js
@@ -14,6 +14,9 @@ const PlaceCard = ({ placeDetails }) => {
     return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=960&photoreference=${photos[0].photo_reference}&key=AIzaSyBJd54lkWdNgrJVYKp4Oqr2YUSkfScT5Rg`;
   }
 
+  const getStars=()=>{
+
+  }
 
 
   return (
@@ -24,11 +27,11 @@ const PlaceCard = ({ placeDetails }) => {
       :
       (
         <div class="col-md-4" style={{display:`flex`}}>
-          <div class="card" style={{ maxWidth: `100%`,maxHeight:`500px`,minWidth:`100%`,minHeight:`500px`, borderColor:`#00293c`,borderStyle: `solid 1 px`}}>
-            <img class="card-img-top" src={getImage(placeDetails.photos)} style={{minHeight:`70%`,maxHeight:`70%`,borderColor:`#00293c`,borderStyle: `double` }}></img>
-            <div class="card-body" style={{fontFamily:`cursive`, fontVariant:`historical-forms`,fontWeight:`bolder`}}>
+          <div class="card" style={{ maxWidth: `100%`,maxHeight:`500px`,minWidth:`100%`,minHeight:`500px`,padding:`1%` }}>
+            <img class="card-img-top" src={getImage(placeDetails.photos)} style={{minHeight:`70%`,maxHeight:`70%` }}></img>
+            <div class="card-body" style={{fontFamxily:`cursive`, fontVariant:`historical-forms`,fontWeight:`bolder`,backgroundColor:`white`, hover:{backgroundColor:`grey`}}}>
               <h5 class="card-title" style={{color:`00293c`,fontWeight:`bolder`}}>{placeDetails.name}</h5>
-              <p class="card-text"> <b>Ratings:</b>{placeDetails.rating} &nbsp;&nbsp;
+              <p class="card-text">Ratings:{placeDetails.rating} &nbsp;&nbsp;
                 User Ratings:{placeDetails.user_ratings_total}</p>
               <p class="card-text">Vicinity:{placeDetails.vicinity}</p>
             </div>
